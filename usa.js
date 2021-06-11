d7a8f76734913a8ac7dac085b80029c04a0ab51a
Kenneth Luce <kcl65_2000@yahoo.com>
2021-06-11 09:44:00 -0400

    Corrected JSON file removing monuments

--- usa.js
+++ usa.js
@@ -1,8 +1,5 @@
-const wrapper = document.getElementById("wrapper");
-const navBar = document.getElementById("navbar");
-const links = document.querySelectorAll(".links");
+const h2 = document.getElementsByTagName("h2");
 const states = document.getElementById("States");
-const randState = document.getElementById("rand-state");
 const stateHeader = document.getElementById("state-header");
 const cap = document.getElementById("cap");
 const reps = document.getElementById("reps");
@@ -10,12 +7,6 @@ const flag = document.getElementById("state-flag");
 const abv = document.getElementById("abv");
 const pop = document.getElementById("pop");
 const ratif = document.getElementById("ratif");
-const h2 = document.getElementsByTagName("h2");
-const body = document.body;
-const dropContent = document.querySelectorAll("drop");
-const category = document.querySelectorAll("category")
-const varArry = [wrapper, navBar, links, states, randState, stateHeader, cap, reps, flag, abv, pop, ratif, h2, body, dropContent];
-const dropElems = document.querySelectorAll(".category .links > li")
 const natMon =
    ["Statue of Liberty",
    "Independence Hall",
@@ -28,8 +19,8 @@ const natMon =
    "World War II Valor in the Pacific",
    "Little Bighorn Battlefield"];
 
-fetch("https://gist.githubusercontent.com/Kenaxion/38448ed83c8dcb23380fc361ed5f554b/raw/925ea6f5720738d57f781bff901e746f63f8846e/code.json")
-  .then(response => response.json()) //JSON.parse(response)
+fetch("https://gist.githubusercontent.com/Kenaxion/38448ed83c8dcb23380fc361ed5f554b/raw/5ac1ad99f2121238411d080f125777ca11accbc4/code.json")
+  .then(response => response.json())
   .then(
     function (data) {
       addStateDropdown(data);
@@ -49,7 +40,5 @@ fetch("https://gist.githubusercontent.com/Kenaxion/38448ed83c8dcb23380fc361ed5f5
    flag.src = "flags/" + randomState.abv + ".svg"; 
    pop.textContent = randomState.population;
    ratif.textContent = randomState.ratified;
-  //  console.log(stateIndex);
-  //  console.log(randomState);
   }
 
